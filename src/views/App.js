debb7dd7ae9701bd12e0a8645570e7bcdeadf693
kamilnr10 <kamil.nr10@gmail.com>
2022-02-23 16:34:13 +0100

    feat: login screen is done

--- src/views/App.js
+++ src/views/App.js
@@ -1,21 +1,40 @@
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from 'views/Login';
 import Register from 'views/Register';
 import Reset from 'views/Reset';
 import Dashboard from 'views/Dashboard';
+import styled, { ThemeProvider } from 'styled-components';
+import { theme } from 'assets/styles/theme';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
+
+const Game = () => {
+  const [player1, serPlayer1] = useState('');
+  return <p>Home</p>;
+};
+
+const Wrapper = styled.div`
+  height: 100vh;
+  padding: 0 20px 0 20px;
+  background-color: #f5f5f5;
+`;
 
 function App() {
   return (
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/reset" element={<Reset />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Wrapper>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/reset" element={<Reset />} />
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            {/* <Route exact path="/dashboard/game" element={<Game />} /> */}
+          </Routes>
+        </Wrapper>
+      </ThemeProvider>
+    </Router>
   );
 }
 export default App;
--- src/views/Login.js
+++ src/views/Login.js
@@ -7,6 +7,61 @@ import {
 } from 'helpers/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import './Login.css';
+import styled from 'styled-components';
+
+const LoginForm = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  background-color: white;
+  padding: 30px;
+  border-radius: 25px;
+  box-shadow: 0px 7px 20px 4px rgba(236, 232, 253, 1);
+  overflow: hidden;
+  z-index: 10;
+`;
+
+const CircleBackground = styled.div`
+  position: absolute;
+  width: 700px;
+  height: 700px;
+  background-color: #f9f5ff;
+  border-radius: 50%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -10%);
+  z-index: -1;
+`;
+
+const LoginTextBox = styled.input`
+  padding: 10px;
+  font-size: 18px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  color: grey;
+  border: 1px solid #6c95bb;
+  box-shadow: inset 0px 6px 20px 4px rgba(209, 220, 232, 1);
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 18px;
+  margin-bottom: 10px;
+  border: none;
+  border-radius: 10px;
+  color: white;
+  background-image: linear-gradient(to right, #756ef4 0%, #be85fa 100%);
+  /* box-shadow: 0px 7px 31px 19px rgba(56, 206, 232, 1); */
+`;
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -15,6 +70,7 @@ const Login = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    console.log(process.env.REACT_APP_FIREBASE_API_KEY);
     if (loading) {
       // maybe trigger a loading screen
       return;
@@ -23,39 +79,40 @@ const Login = () => {
   }, [user, loading]);
 
   return (
-    <div className="login">
-      <div className="login__container">
-        <input
+    <LoginForm>
+      <LoginContainer>
+        <LoginTextBox
           type="text"
           className="login__textBox"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="E-mail Address"
         />
-        <input
+        <LoginTextBox
           type="password"
           className="login__textBox"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
         />
-        <button
+        <Button
           className="login__btn"
           onClick={() => logInWithEmailAndPassword(email, password)}
         >
           Login
-        </button>
-        <button className="login__btn login__google" onClick={signInWithGoogle}>
+        </Button>
+        <Button className="login__btn login__google" onClick={signInWithGoogle}>
           Login with Google
-        </button>
+        </Button>
         <div>
           <Link to="/reset">Forgot Password</Link>
         </div>
         <div>
           Don't have an account? <Link to="/register">Register</Link> now.
         </div>
-      </div>
-    </div>
+        <CircleBackground />
+      </LoginContainer>
+    </LoginForm>
   );
 };
 export default Login;
