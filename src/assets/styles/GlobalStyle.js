28bdd7ecbe24e298349ee815cd4776240230d037
kamilnr10 <kamil.nr10@gmail.com>
2022-03-19 16:27:19 +0100

    feat:add routing to cryptocurrency detail

--- src/assets/styles/GlobalStyle.js
+++ src/assets/styles/GlobalStyle.js
@@ -11,13 +11,15 @@ body {
     font-family: 'Montserrat', sans-serif;
     margin: 0;
     padding: 0;
+    color: white;
+    background-color: #0c0a11;
     ${
       '' /* background-color: #232328;
      background-image: linear-gradient( #29323c 0%, #485563 100%);  */
     }
-     background: #000000;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(to bottom, #434343, #000000);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to bottom, #434343, #000000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    /* background: #000000;  
+    background: -webkit-linear-gradient(to bottom, #434343, #000000);  
+    background: linear-gradient(to bottom, #434343, #000000); */
 
 
 }
@@ -29,7 +31,24 @@ body {
     
 }
 
+th {
 
+}
+
+th div {
+
+    svg {
+        font-size: 6px !important; 
+    }
+}
+
+th div span:hover {
+color: #84ff9d !important;
+}
+
+.MuiTableSortLabel-active {
+    color: #29fd53 !important;
+}
 
 a,button {
     font-family: 'Montserrat', sans-serif;
--- src/views/AuthenticatedApp/AuthenticatedApp.js
+++ src/views/AuthenticatedApp/AuthenticatedApp.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Dashboard from 'components/templates/Dashboard/Dashboard';
 import { MainTemplate } from 'components/templates/MainTemplate/MainTemplate';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ProtectedRoute } from 'views/ProtectedRoute';
+import { Route, Routes } from 'react-router-dom';
 import Portfolio from 'components/templates/Portfolio/Portfolio';
 import Search from 'components/templates/Search/Search';
 import Explore from 'components/templates/Explore/Explore';
 import More from 'components/templates/More/More';
+import { CoinDetail } from 'components/templates/CoinDetail/CoinDetail';
 
 export const AuthenticatedApp = () => {
   return (
     <MainTemplate>
       <Routes>
         <Route path="dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/:id" element={<CoinDetail />} />
         <Route path="portfolio" element={<Portfolio />} />
         <Route path="search" element={<Search />} />
         <Route path="explore" element={<Explore />} />
