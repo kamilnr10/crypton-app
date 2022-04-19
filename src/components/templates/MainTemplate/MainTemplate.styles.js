94344995c12fb64887f6d0a637d74ae30af779c9
kamilnr10 <kamil.nr10@gmail.com>
2022-04-19 21:43:18 +0200

    fix: taskbar is below url bar on mobile view

--- src/components/templates/MainTemplate/MainTemplate.styles.js
+++ src/components/templates/MainTemplate/MainTemplate.styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const MainTemplateWrapper = styled.div`
+  min-height: -webkit-fill-available;
   height: 100vh;
   display: grid;
   grid-template-rows: 50px 1fr 50px;
